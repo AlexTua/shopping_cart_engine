7b983c0582a17f34ac2d541d0df1e65cdeac16c4
AlexTua <taler97@gmail.com>
2017-04-15 17:10:34 +0300

    branch dev

---
+++ app/mailers/shopping_cart/checkout_mailer.rb
@@ -0,0 +1,9 @@
+module ShoppingCart
+  class CheckoutMailer < ApplicationMailer
+     def complete_email(user, order)
+      @user = user
+      @order = order
+      mail(to: @user.email, subject: "Your order #{@order.track_number} is accepted")
+    end
+  end
+end
